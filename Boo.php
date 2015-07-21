518efd64da82e5b9a18667331b5e8123a13facd1
Alisson Souza <alisson.souza2@gmail.com>
2015-07-21 19:14:47 -0300

    fix some content

--- Boo.php
+++ Boo.php
@@ -1,3 +1,8 @@
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-Donec vestibulum vehicula nisl, non euismod ipsum sodales et. Nullam interdum leo mattis scelerisque commodo. 
-Integer iaculis sit amet turpis ut posuere. Nulla tempor nec enim a sollicitudin
+Donec vestibulum vehicula nisl, non euismod . Nullam interdum leo mattis scelerisque commodo. 
+Integer iaculis sit amet turpis ut posuere. 
+
+Donec vestibulum vehicula nisl, non euismod . Nullam interdum leo mattis scelerisque commodo. 
+Integer iaculis sit amet turpis ut posuere. 
+
+Nulla tempor nec enim a sollicitudin
